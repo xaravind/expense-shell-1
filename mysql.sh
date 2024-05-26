09a54ef4df11124a36dcb994a33b06f17bdb7429
xaravind <aravindbasava1@gmail.com>
2024-05-26 17:26:09 +0530

    db

--- mysql.sh
+++ mysql.sh
@@ -9,13 +9,13 @@ read -s mysql_root_password
 check_root
 
 dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing my sql server"
+
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "enabling my sql server"
+
 
 systemctl start mysqld &>>$LOGFILE 
-VALIDATE $? "starting my sql server"
+
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "setting up root server"
